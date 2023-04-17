a1e4aaad3c5bc8a72da94528d704f3c5f54b82a3
Delos Alvin <alvindelos439@gmail.com>
2023-04-17 01:43:28 -0700

    Increase ranking by 10

--- movies/tasks.py
+++ movies/tasks.py
@@ -1,6 +1,10 @@
+from django.db import models
 from celery import shared_task
+from .models import Movie, MovieStatus
 
 
-@shared_task()
+@shared_task
 def update_movie_ranking():
-    print("here")
+    # Get all movies with status="coming-up"
+    coming_up_movies = Movie.objects.filter(status=MovieStatus.COMINGUP)
+    coming_up_movies.update(ranking=models.F("ranking") + 10)
